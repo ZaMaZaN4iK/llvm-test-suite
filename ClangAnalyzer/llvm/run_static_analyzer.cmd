88b653296e3950d01d45d14b31cd5027e6511b6b
Alexander Zaitsev <zamazan4ik@tut.by>
2020-01-11 22:28:29 +0300

    Fix LLVM build

--- ClangAnalyzer/llvm/run_static_analyzer.cmd
+++ ClangAnalyzer/llvm/run_static_analyzer.cmd
@@ -1,3 +1,3 @@
-cmake -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;lldb;compiler-rt;lld;polly;libunwind" .
+cmake -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;compiler-rt;lld;polly;libunwind" llvm
 make clean
 make -j13
